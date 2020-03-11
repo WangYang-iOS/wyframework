b9e6ef800ed2cab5f6deaa36c9c6672e0ef4fe36
wy <13516829309@163.com>
2020-03-11 16:12:05 +0800

    123

---
+++ wyframework/include/wysdk/wysdk.h
@@ -0,0 +1,15 @@
+//
+//  wysdk.h
+//  wysdk
+//
+//  Created by wangyang on 2020/3/11.
+//  Copyright © 2020 wy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface wysdk : NSObject
+
++ (void)wyRun;
+
+@end
